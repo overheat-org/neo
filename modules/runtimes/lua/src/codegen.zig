ef2c3e40ea108957e9fc2600686b98577505f89c
NexusPrime <nicolasestevaolima104@gmail.com>
2025-07-10 09:26:50 -0300

    feat: remake lua codegen

--- modules/runtimes/lua/src/codegen.zig
+++ modules/runtimes/lua/src/codegen.zig
@@ -2,47 +2,89 @@ const std = @import("std");
 const neo = @import("neo");
 const NeoError = neo.reporter;
 const Node = neo.node;
-const format = neo.utils.format;
+const unwrap_error = neo.utils.unwrap_error;
 
-const allocator = std.heap.page_allocator;
+const Codegen = @This();
 
-pub fn codegen(node: *Node, parent: ?*Node) []u8 {
-	return switch (node.kind) {
-		.Program => {
-			const a = [10].{};
-			
-			for (node.children) |child| {
-				child;codegen(node.children[0], null)
-			}
-		},
-        .If => {
-            const props = node.props.If;
-            const expect = codegen(props.expect, null);
-            const then = codegen(props.then, null);
-            
-            const children = if (props.children) |child| blk: {
-                if (child.kind == .If) {
-                    break :blk format("{s} end", .{codegen(child, node)});
-                }
-                break :blk format("{s} end", .{codegen(child, null)});
-            } else "end";
-            
-            return if (parent == null or parent.?.kind != .If)
-                format("if {s} then {s} {s}", .{ expect, then, children })
-            else
-                format("elseif {s} then {s} {s}", .{ expect, then, children });
-        },
-		.BinaryExpression => node.repr(),
-		.Block => format("do {s} end", .{"TODO"}),
-		.Number => node.repr(),
-		.String => node.repr(),
-		.Null => format("nil", .{}),
-		else => unreachable,
+buffer: std.ArrayList(u8),
+allocator: std.mem.Allocator,
+
+pub fn init(allocator: std.mem.Allocator) Codegen {
+	return .{
+		.allocator = allocator,
+		.buffer = std.ArrayList(u8).init(allocator),
 	};
 }
 
+pub fn deinit(self: *Codegen) void {
+	self.buffer.deinit();
+}
+
+pub fn generate(self: *Codegen, ast: *Node) []u8 {
+	self.traverse(ast);
+	return unwrap_error(self.buffer.toOwnedSlice());
+}
+
+fn write(self: *Codegen, text: []const u8) void {
+	unwrap_error(self.buffer.appendSlice(text));
+}
+
+fn traverse(self: *Codegen, node: *Node) void {
+	defer node.destroy();
+
+	switch (node.kind) {
+		.Program => {
+			for (node.children) |child| {
+				traverse(self, child);
+			}
+		},
+		.If => {
+			const props = node.props.If;
+			
+			self.write("if ");
+			traverse(self, props.expect);
+			self.write(" then ");
+			traverse(self, props.then);
+
+			var children = props.children;
+			while (children) |child| {
+				if (child.kind == .If) {
+					self.write("\nelseif ");
+					traverse(self, child);
+				} else {
+					self.write("\nelse ");
+					traverse(self, child);
+				}
+				children = child.children;
+			}
+
+			self.write("\nend\n");
+		},
+		.BinaryExpression => {
+			return node.repr();
+		},
+		.Block => { 
+			self.write("do\n");
+			for (node.children) |child| {
+				traverse(self, child);
+			}
+			self.write("\nend\n");
+		},
+		.Number => {
+			node.repr();
+		},
+		.String => {
+			node.repr();
+		},
+		.Null => {
+			self.write("nil");
+		},
+		else => unreachable,
+	}
+}
+
 test {
-	const code = codegen(Node.new(.If, .{
+	const ast = Node.new(.If, .{
 		.expect = Node.new(.Number, .{ .value = 5 }),
 		.then = Node.new(.Null, .{}),
 		.children = Node.new(.If, .{
@@ -50,7 +92,10 @@ test {
 			.then = Node.new(.Null, .{}),
 			.children = null
 		})
-	}), null);
+	});
+
+	const codegen = Codegen.init(std.testing.allocator);
+	const code = codegen.generate(ast);
 	
 	std.debug.print(
 		"{s}",
